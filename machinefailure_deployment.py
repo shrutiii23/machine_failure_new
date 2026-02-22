9cb9addab97a359bc1b22bc43bbe9a6a12a84a2c
shrutiii23 <shruti.nmfdegree@gmail.com>
2026-02-22 14:38:47 +0530

    Update machinefailure_deployment.py

--- machinefailure_deployment.py
+++ machinefailure_deployment.py
@@ -14,6 +14,8 @@ import pandas as pd
 model=joblib.load("dtc_model (1).pkl")
 
 st.title("machine failure prediction !")
+print("Model features:", model.feature_names_in_)
+print("Input features:", input_data.columns)
 
 footfall =st.number_input("footfall")
 tempMode=st.number_input("tempMode")
